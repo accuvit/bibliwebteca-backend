68217129ed571a2cd8ac605a77425d1d1e55827d
accuvit <laccuvit@gmail.com>
2021-10-20 02:25:23 -0300

    docker

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const livrosRouter = require('./routes/livros');
 
 const app = express();
+
+app.use(cors());
 
 main()
   .then(() => console.log('mongodb conected!'))
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.DATABASE_URL);
+  await mongoose.connect(process.env.ME_CONFIG_MONGODB_URL);
 }
 
 app.use(express.json());
 app.use('/', livrosRouter);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-  console.log(`Iniciando conexão em ${process.env.DATABASE_URL}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Iniciando conexão em ${process.env.ME_CONFIG_MONGODB_URL}`);
 });
